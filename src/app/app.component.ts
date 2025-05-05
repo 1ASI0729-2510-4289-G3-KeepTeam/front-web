e144a4287cc2c96d507cb583cbaf3c8f11f3ea90
yigabriel <gabrielsacc7@gmail.com>
2025-05-05 01:16:03 -0500

    fix: wrong html in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {ToolbarComponent} from './public/components/toolbar/toolbar.component';
+import {WishItemComponent} from './collections/pages/wish-item-page/wish-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ToolbarComponent],
+  imports: [ToolbarComponent, WishItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
