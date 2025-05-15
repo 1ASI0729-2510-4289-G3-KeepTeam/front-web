b7e2f1a9d332e2eb6e7f9329594ee853c48eb392
maria-mm0907 <U202317442@upc.edu.pe>
2025-05-15 17:46:37 -0500

    feat: add plans route to toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,14 @@ import {NgModule} from '@angular/core';
 import {LoginComponent} from './public/pages/login/login-content/login-content.component';
 import {SignUpComponent} from './public/pages/signUp/sign-up-content/sign-up-content.component';
 import {UserProfileComponent} from './profiles/pages/user-profile/user-profile.component';
-
+import {PlansContentComponent} from './public/pages/plans/plans-content/plans-content.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
   {path: 'user-profile', component: UserProfileComponent },
+  {path: 'plans', component: PlansContentComponent },
 ];
 
 @NgModule({
--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,6 +9,7 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarRow,
     MatToolbar,
     MatIconModule,
+    RouterLink,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
